292efeedcbcdb7d0f4ccb621b64390bc4d3c54d3
Nishant Rawat <nishantrawat22@gmail.com>
2018-02-19 17:36:57 -0700

    Reverse bits using swap - not working

--- src/primitives/ReverseBits.java
+++ src/primitives/ReverseBits.java
@@ -8,4 +8,13 @@ public class ReverseBits {
 		//Convert back to int
 		return Integer.parseInt(s,2);
 	}
+	
+	public int usingSwaps(int x){
+		//TODO
+		SwapBits s = new SwapBits();
+		for(int i=0;i<=31;i++){
+			x = s.optimizedBruteForce(x, 31-i, i);
+		}
+		return x;
+	}
 }
--- tst/primitives/ReverseBitsTest.java
+++ tst/primitives/ReverseBitsTest.java
@@ -9,5 +9,11 @@ public class ReverseBitsTest {
 	public void testReversedBitsUsingBruteForce(){
 		Assert.assertEquals(3, new ReverseBits().usingBruteForce(12));
 	}
+	
+	@Test
+	public void testReversedStringUsingSwappedBits(){
+		//Need to fix
+		//Assert.assertEquals(3, new ReverseBits().usingSwaps(12));
+	}
 
 }
