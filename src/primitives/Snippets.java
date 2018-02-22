5a717bc7eb65c94cb8b6396286e1e91006991bfe
Nishant Rawat <nishantrawat22@gmail.com>
2018-02-22 10:14:04 -0700

    Reverse digits

--- src/primitives/Snippets.java
+++ src/primitives/Snippets.java
@@ -12,5 +12,21 @@ public class Snippets {
 	public int getIndexOfLowestSetBit(int x){
 		return -1;
 	}
+	
+	public int reverseDigits(int x){
+		int reverseX = 0;
+		int absX = Math.abs(x);
+		while(absX != 0){
+			/**
+			 * If number is 1 digit long reverseX*10 will be 0, hence returning x%10.
+			 * But for longer numbers, every time the loop starts again, we increase the position of the
+			 * previous digit by 10 by multiplying it with 10, so 3 becomes 30, 30 becomes 300
+			 */
+			reverseX = reverseX*10 + absX%10; 
+			absX = absX/10;
+		}
+		
+		return x>0?reverseX:-reverseX;
+	}
 
 }
--- tst/primitives/SnippetsTest.java
+++ tst/primitives/SnippetsTest.java
@@ -10,5 +10,17 @@ public class SnippetsTest {
 		Snippets s = new Snippets();
 		Assert.assertEquals(32, s.clearLowestSetBit(48));
 	}
+	
+	@Test
+	public void testPositiveReverseDigits(){
+		Snippets s = new Snippets();
+		Assert.assertEquals(184, s.reverseDigits(481));
+	}
+	
+	@Test
+	public void testNegativeReverseDigits(){
+		Snippets s = new Snippets();
+		Assert.assertEquals(-184, s.reverseDigits(-481));
+	}
 
 }
